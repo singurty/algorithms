baddc5f360d8c0a0983f588d81c8c1bdba843455
singurty <singurty@gmail.com>
2021-11-12 20:22:55 +0545

    solve reverse singly linked list

---
+++ data-structures/reverse.c
@@ -0,0 +1,58 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#define LENGTH 10
+
+typedef struct {
+	int item;
+	struct list *next;
+} list;
+
+list *head;
+
+void insert(int item) {
+	list *p;
+	p = malloc(sizeof(list));
+	p->item = item;
+	p->next = (struct list *) head;
+	head = p;
+}
+
+void printList() {
+	list *l = head;
+	while (l != NULL) {
+		printf("%d\n", l->item);
+		l = (list *) l->next;
+	}
+}
+
+void reverse() {
+	list** pointers = malloc(LENGTH);
+	list *l = head;
+	// get pointers of all nodes
+	pointers[0] = head;
+	for (int i = 1; l->next != NULL; i++) {
+		pointers[i] = (list *) l->next;
+		l = (list *) l->next;
+	}
+	// put them back in reverse order
+	head = pointers[LENGTH - 1];
+	l = head;
+	for (int i = LENGTH - 2; i >= 0; i--) {
+		l->next = (struct list *) pointers[i];
+		l = (list *) l->next;
+	}
+	// mark the end
+	pointers[0]->next = NULL;
+}
+
+int main() {
+	head = NULL;
+	for (int i = 0; i < LENGTH; i++) {
+		insert(i);
+	}
+	printList();
+	reverse();
+	printf("After reverse:\n");
+	printList();
+}
