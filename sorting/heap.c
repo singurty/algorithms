ac05bdb389a66dce18dd8bb05c9071393c7e0177
singurty <singurty@gmail.com>
2021-12-06 10:47:51 +0545

    implement heap data structure

---
+++ sorting/heap.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+#define PQ_SIZE 20
+
+typedef struct {
+	int q[PQ_SIZE];
+	int n;
+} priority_queue;
+
+int pq_parent(int n) {
+	if (n == 0) {
+		return -1;
+	}
+	return (int) n/2;
+}
+
+int pq_young_child(int n) {
+	return 2 * n;
+}
+
+void pq_swap(priority_queue *q, int x, int y) {
+	int temp = q->q[x];
+	q->q[x] = q->q[y];
+	q->q[y] = temp;
+}
+
+void bubble_up(priority_queue *q, int p) {
+	if (pq_parent(p) == -1) {
+		return; // root of heap
+	}
+	if (q->q[pq_parent(p)] > q->q[p]) {
+		pq_swap(q, p, pq_parent(p));
+		bubble_up(q, pq_parent(p));
+	}
+}
+
+void pq_insert(priority_queue *q, int x) {
+	if (q->n >= PQ_SIZE) {
+		printf("Warning: priority queue overflow!\n");
+	} else {
+		q->n = (q->n) + 1;
+		q->q[q->n] = x;
+		bubble_up(q, q->n);
+	}
+}
+
+void pq_init(priority_queue *q) {
+	q->n = 0;
+}
+
+void make_heap(priority_queue *q, int s[], int n) {
+	pq_init(q);
+	for (int i = 0; i < n; i++) {
+		pq_insert(q, s[i]);
+	}
+}
